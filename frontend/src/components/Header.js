042aa06b4511364545c1478af29f0d23872ad03f
waldemarciupa <waldemarciupa90@gmail.com>
2020-10-15 21:46:03 +0200

    implemented react-bootstrap and styled header and footer

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Container, Navbar, Nav } from 'react-bootstrap';
 
 const Header = () => {
     return (
         <header>
-            header
+            <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+                <Container>
+                    <Navbar.Brand href="/">ProShop</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ml-auto">
+                            <Nav.Link href="/cart">Cart</Nav.Link>
+                            <Nav.Link href="/login">Sign In</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </header>
     )
 }
