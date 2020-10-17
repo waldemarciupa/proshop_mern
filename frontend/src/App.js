a51e6f46ddf068c28830decbce37af870aa2eb94
waldemarciupa <waldemarciupa90@gmail.com>
2020-10-17 16:54:44 +0200

    implemented react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -11,7 +11,7 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
-          <HomeScreen />
+          <Route path='/' component={HomeScreen} exact />
         </Container>
       </main>
       <Footer />
