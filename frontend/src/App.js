f03d8bb9c4e20fa41e7a8304054bac052ed12bdb
waldemarciupa <waldemarciupa90@gmail.com>
2020-10-17 16:53:29 +0200

    implemented react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -6,7 +7,7 @@ import HomeScreen from './screens/HomeScreen';
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className='py-3'>
         <Container>
@@ -14,7 +15,7 @@ const App = () => {
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 }
 
