35aa9ee1b3303d36f3cb0c4f40e5eff58757394a
waldemarciupa <waldemarciupa90@gmail.com>
2020-10-27 06:19:44 +0100

    bringing redux state into home screen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -7,11 +7,12 @@ import { listProducts } from '../actions/productAction';
 const HomeScreen = () => {
     const dispatch = useDispatch();
 
+    const productList = useSelector(state => state.productList)
+    const { loading, error, products } = productList
+
     useEffect(() => {
         dispatch(listProducts())
     }, [dispatch])
-
-    const products = []
 
     return (
         <>
