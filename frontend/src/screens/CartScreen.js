5c585899987ca689a8657839cad363b224d2da92
waldemarciupa <waldemarciupa90@gmail.com>
2020-11-03 06:41:44 +0100

    created cart screen

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -5,8 +5,21 @@ import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
 import Message from '../components/Message'
 import { addToCart } from '../actions/cartActions'
 
+const CartScreen = ({ match, location, history }) => {
+    const productId = match.params.id
 
-const CartScreen = () => {
+    const qty = location.search ? Number(location.search.split('=')[1]) : 1
+
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        if (productId) {
+            dispatch(addToCart(productId, qty))
+        }
+    }, [dispatch, productId, qty])
+
+    console.log(qty);
+
     return (
         <div>
             Cart
