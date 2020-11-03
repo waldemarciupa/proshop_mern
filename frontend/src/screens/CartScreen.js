d2dc6bdb9f65a3d70e312855519362633bf2332f
waldemarciupa <waldemarciupa90@gmail.com>
2020-11-03 06:43:38 +0100

    created cart screen

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -7,10 +7,10 @@ import { addToCart } from '../actions/cartActions'
 
 const CartScreen = ({ match, location, history }) => {
     const productId = match.params.id
-
     const qty = location.search ? Number(location.search.split('=')[1]) : 1
-
     const dispatch = useDispatch()
+    const cart = useSelector(state => state.cart)
+    const { cartItems } = cart
 
     useEffect(() => {
         if (productId) {
