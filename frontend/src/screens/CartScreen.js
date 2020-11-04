c09c0605ed690909b4f482e248f7198938e591c0
waldemarciupa <waldemarciupa90@gmail.com>
2020-11-04 07:34:50 +0100

    created cart screen

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -22,6 +22,10 @@ const CartScreen = ({ match, location, history }) => {
         console.log('remove');
     }
 
+    const checkoutHandler = () => {
+        history.push('/login?redirect=shipping')
+    }
+
     return (
         <Row>
             <Col md={8}>
@@ -72,9 +76,28 @@ const CartScreen = ({ match, location, history }) => {
                         </ListGroup>
                     )}
             </Col>
-            <Col md={2}>
-            </Col>
-            <Col md={2}>
+            <Col md={4}>
+                <Card>
+                    <ListGroup variant='flush'>
+                        <ListGroup.Item>
+                            <h2>Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)}) items</h2>
+                            ${cartItems
+                                .reduce((acc, item) => acc + item.qty * item.price, 0)
+                                .toFixed(2)}
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <Button
+                                type='button'
+                                className='btn-block'
+                                disabled={cartItems.length === 0}
+                                onClick={checkoutHandler}
+                            >
+                                Proceed To Checkout
+                            </Button>
+                        </ListGroup.Item>
+
+                    </ListGroup>
+                </Card>
             </Col>
         </Row>
     )
