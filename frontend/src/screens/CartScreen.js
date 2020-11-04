735143d036cd371590867dc5d7afedba541bd7c5
waldemarciupa <waldemarciupa90@gmail.com>
2020-11-04 06:35:23 +0100

    created cart screen

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -21,9 +21,22 @@ const CartScreen = ({ match, location, history }) => {
     console.log(qty);
 
     return (
-        <div>
-            Cart
-        </div>
+        <Row>
+            <Col md={8}>
+                <h1>Shopping Cart</h1>
+                {cartItems.length === 0 ? (
+                    <Message>
+                        Your cart is empty <Link to='/'>Go Back</Link>
+                    </Message>
+                ) : (
+                        <ListGroup variant='flush'></ListGroup>
+                    )}
+            </Col>
+            <Col md={2}>
+            </Col>
+            <Col md={2}>
+            </Col>
+        </Row>
     )
 }
 
