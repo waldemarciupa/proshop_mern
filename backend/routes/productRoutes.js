b0f284beac598de05c491e1e531c230f8705d7e4
waldemarciupa <waldemarciupa90@gmail.com>
2020-11-06 06:34:58 +0100

    created product controller

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import asyncHandler from 'express-async-handler';
 const router = express.Router();
+import asyncHandler from 'express-async-handler';
 import Product from '../models/productModel.js';
 
 
