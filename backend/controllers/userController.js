18e0c67addd7048e7750b876215f4e0ea42b3c9e
waldemarciupa <waldemarciupa90@gmail.com>
2020-11-08 16:50:29 +0100

    working on authorization middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
 import asyncHandler from 'express-async-handler';
 import generateToken from '../utils/generateToken.js';
-import User from '../models/UserModel.js';
+import User from '../models/userModel.js';
+
 
 // @desc    Auth user & get token
 // @route   POST /api/users/login
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -28,6 +28,6 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password)
 }
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema)
 
 export default User;
