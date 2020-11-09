a10f50a6ef151ae48e10aef3bb12eef1c2c7a483
waldemarciupa <waldemarciupa90@gmail.com>
2020-11-09 06:52:38 +0100

    created getuserprofile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,7 +2,6 @@ import asyncHandler from 'express-async-handler';
 import generateToken from '../utils/generateToken.js';
 import User from '../models/userModel.js';
 
-
 // @desc    Auth user & get token
 // @route   POST /api/users/login
 // @access  Public
@@ -29,7 +28,19 @@ const authUser = asyncHandler(async (req, res) => {
 // @route   GET /api/users/profile
 // @access  Private
 const getUserProfile = asyncHandler(async (req, res) => {
-    res.send('Success')
+    const user = await User.findById(req.user._id)
+
+    if (user) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        })
+    } else {
+        res.status(404)
+        throw new Error('User not found!')
+    }
 })
 
 export { authUser, getUserProfile }
