d9435a9fb7c3d1fa8cd2599a7972d4c5f35c8a05
waldemarciupa <waldemarciupa90@gmail.com>
2020-11-11 11:01:47 +0100

    created login screen

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -5,15 +5,45 @@ import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
 import { login } from '../actions/userActions'
+import FormContainer from '../components/FormContainer'
 
 const LoginScreen = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
     return (
-        <div>
+        <FormContainer>
+            <h1>Sign In</h1>
+            <Form onSubmit={submitHandler}>
+                <Form.Group controlId='email'>
+                    <Form.Label>Email Adress</Form.Label>
+                    <Form.Control
+                        type='email'
+                        placeholder='Enter email'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    ></Form.Control>
+                </Form.Group>
+                <Form.Group controlId='password'>
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control
+                        type='password'
+                        placeholder='Enter password'
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    ></Form.Control>
+                </Form.Group>
+                <Button type='submit' variant='primary'>
+                    Sign In
+                </Button>
+            </Form>
+            <Row className='py-3'>
+                <Col>
+                    NewCustomer? <Link to={redirect ? `/register?redirect=${redirect}` : '/register'}>Register</Link>
+                </Col>
 
-        </div>
+            </Row>
+        </FormContainer>
     )
 }
 
