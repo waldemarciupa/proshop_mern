8b171fa26b9cf1c44fb6c68e6245bf814ef08696
waldemarciupa <waldemarciupa90@gmail.com>
2020-11-20 06:46:57 +0100

    created shipping screen

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Form, Button } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import FormContainer from '../components/FormContainer'
 
-const ShippingScreen = () => {
+const ShippingScreen = ({ history }) => {
+
     return (
         <div>
             Shipping
