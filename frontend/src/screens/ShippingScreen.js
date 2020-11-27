5d2b29371116a798e8e565b88a636aa1bb18764c
waldemarciupa <waldemarciupa90@gmail.com>
2020-11-27 06:53:20 +0100

    created shipping screen

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -4,11 +4,15 @@ import { useDispatch, useSelector } from 'react-redux'
 import FormContainer from '../components/FormContainer'
 
 const ShippingScreen = ({ history }) => {
-    const [address, setAdress] = useState('')
+    const [address, setAddress] = useState('')
     const [city, setCity] = useState('')
     const [postalCode, setPostalCode] = useState('')
     const [country, setCountry] = useState('')
 
+    const submitHandler = (e) => {
+        e.preventDefault()
+        console.log('submit');
+    }
 
     return (
         <FormContainer>
@@ -54,6 +58,9 @@ const ShippingScreen = ({ history }) => {
                         onChange={(e) => setCountry(e.target.value)}
                     ></Form.Control>
                 </Form.Group>
+                <Button type='submit' variant='primary'>
+                    Continue
+                </Button>
             </Form>
         </FormContainer>
     )
