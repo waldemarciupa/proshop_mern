46b4e7965f117697808b3e3b847284f08d2426d4
waldemarciupa <waldemarciupa90@gmail.com>
2020-11-30 08:24:40 +0100

    created payment screen

--- frontend/src/screens/PaymentScreen.js
+++ frontend/src/screens/PaymentScreen.js
@@ -31,18 +31,18 @@ const PaymentScreen = ({ history }) => {
             <Form onSubmit={submitHandler}>
                 <Form.Group>
                     <Form.Label as='legend'>Select Method</Form.Label>
+                    <Col>
+                        <Form.Check
+                            type='radio'
+                            label='PayPal or Credit Card'
+                            id='PayPal'
+                            name='paymentMethod'
+                            value='PayPal'
+                            checked
+                            onChange={(e) => setPaymentMethod(e.target.value)}
+                        ></Form.Check>
+                    </Col>
                 </Form.Group>
-                <Col>
-                    <Form.Check
-                        type='radio'
-                        label='PayPal or Credit Card'
-                        id='PayPal'
-                        name='paymentMethod'
-                        value='PayPal'
-                        checked
-                        onChange={(e) => setPaymentMethod(e.target.value)}
-                    ></Form.Check>
-                </Col>
                 <Button type='submit' variant='primary'>
                     Continue
                 </Button>
