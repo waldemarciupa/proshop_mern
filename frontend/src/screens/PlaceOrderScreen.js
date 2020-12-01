4f2bbe4eeae8d33da99783ba8b66fa7fc6f7caaf
waldemarciupa <waldemarciupa90@gmail.com>
2020-12-01 06:39:04 +0100

    created place order screen

---
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+import { Button, Row, Col, ListGroup, Image, Card } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import Message from '../components/Message'
+import CheckoutSteps from '../components/CheckoutSteps'
+
+const PlaceOrderScreen = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default PlaceOrderScreen
