c71d25847335093f94189a5aca19c99c140fc382
waldemarciupa <waldemarciupa90@gmail.com>
2020-12-07 07:10:20 +0100

    createde order screen

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -36,17 +36,34 @@ const OrderScreen = ({ match }) => {
                         <ListGroup variant='flush'>
                             <ListGroup.Item>
                                 <h2>Shipping</h2>
-                                <strong>Name: </strong> {order.user.name}
-                                <a href={`mailto:${order.user.email}`}>{order.user.email}</a>
                                 <p>
-                                    <strong>Address:</strong>
+                                    <strong>Name: </strong> {order.user.name}
+                                </p>
+                                <p>
+                                    <strong>Email: </strong>
+                                    <a href={`mailto:${order.user.email}`}> {order.user.email}</a>
+                                </p>
+                                <p>
+                                    <strong>Address: </strong>
                                     {order.shippingAddress.address}, {order.shippingAddress.city} {order.shippingAddress.postalCode},{' '}{order.shippingAddress.country}
                                 </p>
+                                {order.isDelivered ? (
+                                    <Message variant='success'>Delivered on {order.deliveredAt}</Message>
+                                ) : (
+                                        <Message variant='danger'>Not delivered</Message>
+                                    )}
                             </ListGroup.Item>
                             <ListGroup.Item>
                                 <h2>Payment Method</h2>
-                                <strong>Method: </strong>
-                                {order.paymentMethod}
+                                <p>
+                                    <strong>Method: </strong>
+                                    {order.paymentMethod}
+                                </p>
+                                {order.isPaid ? (
+                                    <Message variant='success'>Paid on {order.paidAt}</Message>
+                                ) : (
+                                        <Message variant='danger'>Not paid</Message>
+                                    )}
                             </ListGroup.Item>
                             <ListGroup.Item>
                                 <h2>Order Items</h2>
