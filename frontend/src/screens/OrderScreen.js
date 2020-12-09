416ce6f5d3ad2317f2c7a8bb4183979fb7529567
waldemarciupa <waldemarciupa90@gmail.com>
2020-12-09 08:20:36 +0100

    adding PayPal payments

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import { Link } from 'react-router-dom'
 import { Button, Row, Col, ListGroup, Image, Card } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
@@ -8,6 +9,9 @@ import { getOrderDetails } from '../actions/orderActions'
 
 const OrderScreen = ({ match }) => {
     const orderId = match.params.id
+
+    const [sdkReady, setSdkReady] = useState(false)
+
     const dispatch = useDispatch()
     const orderDetails = useSelector(state => state.orderDetails)
     const { order, loading, error } = orderDetails
@@ -22,6 +26,18 @@ const OrderScreen = ({ match }) => {
     }
 
     useEffect(() => {
+        const addPayPalScript = async () => {
+            const { data: clientId } = await axios.get('/api/config/paypal')
+            const script = document.createElement('script')
+            script.type = 'text/javascript'
+            script.src = `https://www.paypal.com/sdk/js?client-id=${clientId}`
+            script.async = true
+            script.onload = () => {
+                setSdkReady(true)
+            }
+            document.body.appendChild(script)
+        }
+
         if (!order || order._id !== orderId) {
             dispatch(getOrderDetails(orderId))
         }
