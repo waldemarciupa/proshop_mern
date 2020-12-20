6ff8c5075f8b1bd8008f374599a995d3cc89efff
waldemarciupa <waldemarciupa90@gmail.com>
2020-12-20 15:27:48 +0100

    created get user by id controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -127,4 +127,17 @@ const deleteUser = asyncHandler(async (req, res) => {
     }
 })
 
+// @desc    Get user by ID
+// @route   GET /api/users/:id
+// @access  Private/Admin
+const getUserById = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id).select('-password')
+    if (user) {
+        res.json(users)
+    } else {
+        res.status(404)
+        throw new Error('User not found')
+    }
+})
+
 export { authUser, registerUser, getUserProfile, updateUserProfile, getUsers, deleteUser }
