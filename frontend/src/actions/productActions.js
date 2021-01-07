3635ba095ce8cff05650950416f062ad9e4ba6a2
waldemarciupa <waldemarciupa90@gmail.com>
2021-01-07 21:29:37 +0100

    created product delete action

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -5,7 +5,10 @@ import {
     PRODUCT_LIST_FAIL,
     PRODUCT_DETAILS_REQUEST,
     PRODUCT_DETAILS_SUCCESS,
-    PRODUCT_DETAILS_FAIL
+    PRODUCT_DETAILS_FAIL,
+    PRODUCT_DELETE_FAIL,
+    PRODUCT_DELETE_SUCCESS,
+    PRODUCT_DELETE_REQUEST
 } from '../constants/productConstants';
 
 export const listProducts = () => async (dispatch) => {
@@ -46,6 +49,44 @@ export const listProductDetails = (id) => async (dispatch) => {
                 error.response && error.response.data.message
                     ? error.response.data.message
                     : error.message
+        })
+    }
+}
+
+
+
+export const deleteProduct = (id) => async (dispatch, getState) => {
+    try {
+        dispatch({
+            type: PRODUCT_DELETE_REQUEST,
+        })
+
+        const {
+            userLogin: { userInfo },
+        } = getState()
+
+        const config = {
+            headers: {
+                Authorization: `Bearer ${userInfo.token}`,
+            },
+        }
+
+        await axios.get(`/api/products/${id}`, config)
+
+        dispatch({
+            type: PRODUCT_DELETE_SUCCESS,
+        })
+    } catch (error) {
+        const message =
+            error.response && error.response.data.message
+                ? error.response.data.message
+                : error.message
+        if (message === 'Not authorized, token failed') {
+            // dispatch(logout())
+        }
+        dispatch({
+            type: PRODUCT_DELETE_FAIL,
+            payload: message,
         })
     }
 }
