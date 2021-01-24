4f2a4d5813cb76b336a1e7df5dcd03c291ef0446
waldemarciupa <waldemarciupa90@gmail.com>
2021-01-24 18:02:16 +0100

    created order deliver reducer

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -15,7 +15,11 @@ import {
     ORDER_LIST_MY_RESET,
     ORDER_LIST_REQUEST,
     ORDER_LIST_SUCCESS,
-    ORDER_LIST_FAIL
+    ORDER_LIST_FAIL,
+    ORDER_DELIVER_REQUEST,
+    ORDER_DELIVER_SUCCESS,
+    ORDER_DELIVER_FAIL,
+    ORDER_DELIVER_RESET
 } from '../constants/orderConstants'
 
 export const orderCreateReducer = (state = {}, action) => {
@@ -91,6 +95,32 @@ export const orderPayReducer = (
     }
 }
 
+export const orderDeliverReducer = (
+    state = {},
+    action
+) => {
+    switch (action.type) {
+        case ORDER_DELIVER_REQUEST:
+            return {
+                loading: true
+            }
+        case ORDER_DELIVER_SUCCESS:
+            return {
+                loading: false,
+                success: true
+            }
+        case ORDER_DELIVER_FAIL:
+            return {
+                loading: false,
+                error: action.payload
+            }
+        case ORDER_DELIVER_RESET:
+            return {}
+        default:
+            return state
+    }
+}
+
 export const orderListMyReducer = (state = { orders: [] }, action) => {
     switch (action.type) {
         case ORDER_LIST_MY_REQUEST:
@@ -115,7 +145,6 @@ export const orderListMyReducer = (state = { orders: [] }, action) => {
             return state
     }
 }
-
 
 export const orderListReducer = (state = { orders: [] }, action) => {
     switch (action.type) {
