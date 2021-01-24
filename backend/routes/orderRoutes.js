16b7caf0ca456d37239a36d286ab13848f015dfb
waldemarciupa <waldemarciupa90@gmail.com>
2021-01-24 17:57:32 +0100

    created update order to deliver controller and route

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -4,8 +4,9 @@ import {
     addOrderItems,
     getOrderById,
     updateOrderToPaid,
+    updateOrderToDelivered,
     getMyOrders,
-    getOrders
+    getOrders,
 } from '../controllers/orderController.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
 
@@ -13,5 +14,6 @@ router.route('/').post(protect, addOrderItems).get(protect, admin, getMyOrders)
 router.route('/myorders').get(protect, getMyOrders)
 router.route('/:id').get(protect, getOrderById)
 router.route('/:id/pay').put(protect, updateOrderToPaid)
+router.route('/:id/deliver').put(protect, admin, updateOrderToDelivered)
 
 export default router
