dbcf667de179f569ce756583a9548ce7f82d469e
waldemarciupa <waldemarciupa90@gmail.com>
2021-01-27 17:49:40 +0100

    fixed bugs related to order list display

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -9,7 +9,7 @@ import Loader from '../components/Loader'
 import { getOrderDetails, payOrder, deliverOrder } from '../actions/orderActions'
 import { ORDER_PAY_RESET, ORDER_DELIVER_RESET } from '../constants/orderConstants'
 
-const OrderScreen = ({ match }) => {
+const OrderScreen = ({ match, history }) => {
     const orderId = match.params.id
 
     const [sdkReady, setSdkReady] = useState(false)
@@ -38,6 +38,11 @@ const OrderScreen = ({ match }) => {
     }
 
     useEffect(() => {
+        if (!userInfo) {
+            history.push('/login')
+        }
+
+
         const addPayPalScript = async () => {
             const { data: clientId } = await axios.get('/api/config/paypal')
             const script = document.createElement('script')
@@ -177,7 +182,7 @@ const OrderScreen = ({ match }) => {
                                     </ListGroup.Item>
                                 )}
                                 {loadingDeliver && <Loader />}
-                                {userInfo.isAdmin && order.isPaid && !order.isDelivered && (
+                                {userInfo && userInfo.isAdmin && order.isPaid && !order.isDelivered && (
                                     <ListGroup.Item>
                                         <Button type='button' className='btn btn-block' onClick={deliverHandler}>
                                             Mark As Delivered
