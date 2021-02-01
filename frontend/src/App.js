6e915b1e2417c569ee713a931b349be8ffd76199
waldemarciupa <waldemarciupa90@gmail.com>
2021-02-01 19:57:59 +0100

    implementing pagination

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -39,7 +39,8 @@ const App = () => {
           <Route path='/product/:id' component={ProductScreen} />
           <Route path='/admin/userlist' component={UserListScreen} />
           <Route path='/admin/user/:id/edit' component={UserEditScreen} />
-          <Route path='/admin/productlist' component={ProductListScreen} />
+          <Route path='/admin/productlist' component={ProductListScreen} exact />
+          <Route path='/admin/productlist/:pageNumber' component={ProductListScreen} exact />
           <Route path='/admin/product/:id/edit' component={ProductEditScreen} />
           <Route path='/admin/orderlist' component={OrderListScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
