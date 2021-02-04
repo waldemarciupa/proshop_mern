94be42b33d53db0e37ff2ef18625248fb5e8b7e7
waldemarciupa <waldemarciupa90@gmail.com>
2021-02-04 17:10:15 +0100

    created get top products controller

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express'
 const router = express.Router()
-import { getProductById, getProducts, deleteProduct, updateProduct, createProduct, createProductReview } from '../controllers/productController.js'
+import { getProductById, getProducts, deleteProduct, updateProduct, createProduct, createProductReview, getTopProducts } from '../controllers/productController.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
 
 router.route('/')
@@ -8,6 +8,7 @@ router.route('/')
     .post(protect, admin, createProduct)
 router.route('/:id/reviews')
     .post(protect, createProductReview)
+router.get('/top', getTopProducts)
 router.route('/:id')
     .get(getProductById)
     .delete(protect, admin, deleteProduct)
