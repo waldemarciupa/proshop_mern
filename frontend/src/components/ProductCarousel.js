af6e7f26a7279aa43dba559c3f8153ccd67d4576
waldemarciupa <waldemarciupa90@gmail.com>
2021-02-04 17:31:37 +0100

    implemented top rated product carousel

--- frontend/src/components/ProductCarousel.js
+++ frontend/src/components/ProductCarousel.js
@@ -1,8 +1,9 @@
 import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { Carousel, Image } from 'react-bootstrap'
-import { Loader } from './Loader'
-import { Message } from './Message'
+import { useDispatch, useSelector } from 'react-redux'
+import Loader from './Loader'
+import Message from './Message'
 import { listTopProducts } from '../actions/productActions'
 
 const ProductCarousel = () => {
