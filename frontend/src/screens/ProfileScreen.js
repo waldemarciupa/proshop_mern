97435c42e1182cab007329fa5d962f27e7032c93
waldemarciupa <waldemarciupa90@gmail.com>
2021-02-14 10:11:31 +0100

    fixing bugs related to update profile

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -6,6 +6,7 @@ import Message from '../components/Message'
 import Loader from '../components/Loader'
 import { getUserDetails, updateUserProfile } from '../actions/userActions'
 import { listMyOrders } from '../actions/orderActions'
+import { USER_UPDATE_PROFILE_RESET } from '../constants/userConstants'
 
 const ProfileScreen = ({ location, history }) => {
     const [name, setName] = useState('')
@@ -33,6 +34,7 @@ const ProfileScreen = ({ location, history }) => {
             history.push('/login')
         } else {
             if (!user || !user.name || success) {
+                dispatch({ type: USER_UPDATE_PROFILE_RESET })
                 dispatch(getUserDetails('profile'))
                 dispatch(listMyOrders())
             } else {
